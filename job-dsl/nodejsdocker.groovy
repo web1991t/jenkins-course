9baf05587a2642dcbe36c128b85a5c19d1882bf2
Ihor <web1991t@gmail.com>
2020-03-05 10:03:03 +0100

    update docker repo name

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('migor/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
@@ -22,5 +22,6 @@ job('NodeJS Docker example') {
             createFingerprints(false)
             skipDecorate()
         }
+
     }
 }
